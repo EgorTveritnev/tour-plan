d34a16858535539c37c77395227d3c480723605c
Egorbasti <85555153+Egorbasti@users.noreply.github.com>
2021-07-19 15:22:13 +0300

    create: slider with swipper slider

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button__next',
+    prevEl: '.slider-button__prev',
+  },
+});
